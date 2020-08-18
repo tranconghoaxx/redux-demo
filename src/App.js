94cade51c98da3d71fc223140afcb19eb0a7dcee
tranconghoaxx <tranhoa300696@gmail.com>
2020-08-18 08:21:35 +0700

    project start redux

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react';
+import Counter from './containers/Counter/Counter';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <Counter/>
+    </div>
+  );
+}
+
+export default App;
