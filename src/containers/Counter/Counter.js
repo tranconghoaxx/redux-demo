df06c1af6c502a4e37317ca522125236292102c9
tranconghoaxx <tranhoa300696@gmail.com>
2020-08-19 02:20:57 +0700

    passing and retrieve lay lai data tu action to reducer retrieve data and change state

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -44,9 +44,9 @@ class Counter extends Component {
             <div>
                 <CounterOutput value={this.props.ctr}/>
                 <CounterControl label="Increment" clicked={this.props.onIncrementCounter}/>
-                <CounterControl label="Decrement" clicked={() => this.counterChangedHandler('dec')}/>
-                <CounterControl label="Add 5" clicked={() => this.counterChangedHandler('add',5)}/>
-                <CounterControl label="Subtract 5" clicked={() => this.counterChangedHandler('sub',5)}/>
+                <CounterControl label="Decrement" clicked={this.props.onDecrementCounter}/>
+                <CounterControl label="Add 10" clicked={this.props.onAddCounter}/>
+                <CounterControl label="Subtract 15" clicked={this.props.onSubtractCounter}/>
             </div>
         )
     }
@@ -59,7 +59,10 @@ const mapStateToProps = state => {
 // gui hanh dong ben trong component DEN PROPS boi redux
 const mapDispatchToProps = dispatch => {
     return {
-        onIncrementCounter: () => dispatch({type: 'INCREMENT'})
+        onIncrementCounter: () => dispatch({type: 'INCREMENT'}),
+        onDecrementCounter: () => dispatch({type: 'DECREMENT'}),
+        onAddCounter: () => dispatch({type: 'ADD',val: 10}),
+        onSubtractCounter: () => dispatch({type: 'SUBTRACT',val:15}),
     };
 };
 export default connect(mapStateToProps,mapDispatchToProps)(Counter);
